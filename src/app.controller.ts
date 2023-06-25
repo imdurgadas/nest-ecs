f1373b2cfd7054b7de2a0aed7c0c874f7c43fed8
imdurgadas <mail@durgadas.in>
2023-06-25 15:30:49 +0530

    Fix health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from './auth/decorator/public.decorator';
 
 @Controller()
 export class AppController {
   @Get('/health')
+  @Public()
   health() {
     return 'OK';
   }
